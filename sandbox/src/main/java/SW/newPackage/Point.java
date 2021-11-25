bdf10d4761dcf33b309d0c1e22357aac5cfde6f5
Andrew Shamuradov <muskulbrain@gmail.com>
2021-11-25 16:42:07 +0700

    Добавил расчет расстояния между точками

--- sandbox/src/main/java/SW/newPackage/Point.java
+++ sandbox/src/main/java/SW/newPackage/Point.java
@@ -1,10 +1,20 @@
 package SW.newPackage;
 
+import java.sql.SQLOutput;
+
 public class Point {
 
   public static void main(String[] args) {
 
-    System.out.println("Hello, world!");
+    NewPoint S = new NewPoint(2,2,7,6);
+
+    System.out.println("Расстояние между точками =" + distance(S));
+  }
+
+  public static double distance (NewPoint S) {
+    return Math.sqrt(Math.pow((S.x2 - S.x1), 2) + Math.pow((S.y2 - S.y1), 2));
+
+
   }
 
 }
