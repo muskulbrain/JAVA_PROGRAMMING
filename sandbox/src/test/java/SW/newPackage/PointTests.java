96533dad9e9018623b25b556685f456ce4ba2e65
Andrew Shamuradov <muskulbrain@gmail.com>
2021-11-27 16:52:19 +0700

    Update PointTests.java

---
+++ sandbox/src/test/java/SW/newPackage/PointTests.java
@@ -0,0 +1,25 @@
+package SW.newPackage;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class PointTests {
+
+  @Test
+  public void testEquality() {
+
+    PointsData S = new PointsData(2,2,7,6);
+    assert S.distance() == 6.4031242374328485;
+
+  }
+
+  @Test
+  public void testInequality () {
+
+    PointsData S = new PointsData(2,2,7,6);
+    assert S.distance() == 10;
+  }
+
+}
