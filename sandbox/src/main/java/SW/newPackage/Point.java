70cdcd1a4217faadf8ccda815686a3c17343b305
Andrew Shamuradov <muskulbrain@gmail.com>
2021-11-27 16:21:53 +0700

    Переименование класса в PointsData, создание функции distance

--- sandbox/src/main/java/SW/newPackage/Point.java
+++ sandbox/src/main/java/SW/newPackage/Point.java
@@ -6,9 +6,15 @@ public class Point {
 
   public static void main(String[] args) {
 
-    NewPoint S = new NewPoint(2,2,7,6);
+    PointsData S = new PointsData(2,2,7,6);
 
-    System.out.println("Расстояние между точками = " + S.distance());
+    System.out.println("Расстояние между точками =" + distance(S));
+  }
+
+  public static double distance (PointsData S) {
+    return Math.sqrt(Math.pow((S.x2 - S.x1), 2) + Math.pow((S.y2 - S.y1), 2));
+
+
   }
 
 }
