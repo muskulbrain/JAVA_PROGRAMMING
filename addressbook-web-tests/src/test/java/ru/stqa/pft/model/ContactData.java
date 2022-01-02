2410beb99afaf5cce7b8ecf6a83dbe623fcd5e6f
Andrew Shamuradov <muskulbrain@gmail.com>
2022-01-02 22:39:58 +0700

    4.6 Сравнение коллекций для контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/model/ContactData.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.model;
 
+import java.util.Objects;
+
 public class ContactData {
   private final String name;
   private final String lastName;
@@ -31,5 +33,25 @@ public class ContactData {
   public String getEmail() {
 
     return email;
+  }
+
+  @Override
+  public String toString() {
+    return "ContactData{" +
+            "name='" + name + '\'' +
+            '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    ContactData that = (ContactData) o;
+    return Objects.equals(name, that.name) && Objects.equals(lastName, that.lastName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, lastName);
   }
 }
