d1e270967a362a56bccd3e74bd8899a426f53150
Andrew Shamuradov <muskulbrain@gmail.com>
2022-09-04 19:04:37 +0700

    Lecture 6.8

--- addressbook-web-tests/src/test/java/ru/stqa/pft/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/tests/ContactCreationTests.java
@@ -7,7 +7,6 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.model.ContactData;
 import ru.stqa.pft.model.Contacts;
-import ru.stqa.pft.model.GroupData;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -26,18 +25,19 @@ public class ContactCreationTests extends TestBase {
   @DataProvider
   public Iterator<Object[]> validContactsFromXml() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    BufferedReader reader = new BufferedReader(new FileReader(new File ("src/test/resources/contacts.xml")));
-    String xml = "";
-    String line = reader.readLine();
-    while(line != null) {
-      xml+= line;
-      line = reader.readLine();
+    try(BufferedReader reader = new BufferedReader(new FileReader(new File ("src/test/resources/contacts.xml")))) {
+      String xml = "";
+      String line = reader.readLine();
+      while(line != null) {
+        xml+= line;
+        line = reader.readLine();
+      }
+      XStream xstream = new XStream();
+      xstream.allowTypes(new Class[]{ ContactData.class });
+      xstream.processAnnotations(ContactData.class);
+      List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+      return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
     }
-    XStream xstream = new XStream();
-    xstream.allowTypes(new Class[]{ ContactData.class });
-    xstream.processAnnotations(ContactData.class);
-    List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
-    return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
   }
 
   @DataProvider
